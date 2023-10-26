0341628f473d7e88f56953ffa112bd36e05f1d94
WoodenMaiden <yann.pomie@laposte.net>
2023-10-27 00:29:38 +0200

    feat: hide and focus on hover
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import CytoscapeComponent from 'react-cytoscapejs';
-import { ElementDefinition } from 'cytoscape';
+import cytoscape, { ElementDefinition } from 'cytoscape';
 import './App.css';
 import data from './data.json';
 import { stylesheet } from './cy-stylesheet';
@@ -39,6 +39,42 @@ function App() {
         style={{ minWidth: '100%', minHeight: '100%', padding: 0 }}
         layout={{ name: 'circle' }}
         stylesheet={stylesheet}
+        cy={(cy: cytoscape.Core) => {
+          cy.on("mouseover", "node", (event) => {
+            const node = event.target;
+            const connectedEdges = node.connectedEdges();
+            const directNeighbors = node.neighborhood();
+
+            // https://js.cytoscape.org/#eles.symmetricDifference
+            // Here we hide all edges that are not connected to the node
+
+            cy.edges()['(-)'](connectedEdges).forEach((edge: any) => {
+              edge.style('line-opacity', 0.01);
+            })
+            
+            cy.nodes()['(-)'](directNeighbors).forEach((n: any) => {
+              n.style('opacity', 0.01);
+            })
+
+            connectedEdges.forEach((e: any) => {
+              e.style('line-opacity', 1);
+            });
+
+            directNeighbors.forEach((n: any) => {
+              n.style('opacity', 1);
+            })
+
+            node.style('opacity', 1);
+
+          });
+
+          cy.on("mouseout", "node", (_) => {
+            cy.$("").forEach((elt: any) => {
+              elt.style('line-opacity', 0.5);
+              elt.style('opacity', 1);
+            })
+          });
+        }}
       />
     </div>
   );
