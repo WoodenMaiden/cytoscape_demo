d4f01b978a295eeb9b73ca9dd4bc13329ea5332f
WoodenMaiden <yann.pomie@laposte.net>
2023-10-26 21:38:15 +0200

    feat: base app
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
+import CytoscapeComponent from 'react-cytoscapejs';
+import { ElementDefinition } from 'cytoscape';
 import './App.css';
-import data from './data.json'
+import data from './data.json';
 
 function App() {
-  console.log(data)
+  const elements = [
+    ...data.types.map<ElementDefinition>((type) => {
+      return {
+        group: 'nodes',
+        data: {
+          id: type,
+          label: type,
+        },
+      };
+    }),
+    ...data.attacks.map<ElementDefinition>((attack) => {
+      return {
+        group: 'edges',
+        data: {
+          id: `${attack.attacker}->${attack.defender}`,
+          source: attack.attacker,
+          target: attack.defender,
+          label: attack.efficiency.toString(),
+          efficiency: attack.efficiency,
+        },
+      };
+    }),
+  ];
 
   return (
     <div className="App">
-     <header><h1>An awesome pokemon visualisation tool</h1></header>
+      <header>
+        <h1>An awesome pokemon visualisation tool</h1>
+      </header>
 
-     <div>
-
-     </div>
+      <CytoscapeComponent
+        className='cy'
+        elements={elements}
+        style={{ minWidth: '100%', minHeight: '100%', padding: 0 }}
+      />
     </div>
   );
 }
